89b6e6d1a54bfb9c7760d2b494b7bf04236eb73f
Habibur Rahman <amarhabibur@gmail.com>
2025-02-21 17:46:28 +0600

    ..

--- src/app/api/job/[id]/route.js
+++ src/app/api/job/[id]/route.js
@@ -1,15 +1,26 @@
 import JobModel from "@/models/jobs-model";
+import mongoose from "mongoose";
 import connectMongo from "../../../../../connectMongo";
 
 export async function GET(request, context) {
-  await connectMongo();
-  const id = context.params.id;
-  console.log(id);
-  // Find the job by ID
-  const isJob = await JobModel.findById(id);
-  if (!isJob) {
-    return Response.json({ message: "Job not found" }, { status: 404 });
+  try {
+    await connectMongo();
+    const { id } = await context.params;
+
+    // Check if the ID is valid (MongoDB ObjectId validation)
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return Response.json({ message: "Invalid job ID" }, { status: 400 });
+    }
+
+    // Find the job by ID
+    const job = await JobModel.findById(id);
+    if (!job) {
+      return Response.json({ message: "Job not found" }, { status: 404 });
+    }
+
+    return Response.json(job, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
   }
-  const job = await JobModel.findById(id);
-  return Response.json(job, { status: 200 });
 }
